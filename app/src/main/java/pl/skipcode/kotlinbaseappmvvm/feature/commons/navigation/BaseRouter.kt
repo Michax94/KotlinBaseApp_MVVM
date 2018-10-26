97bf78e003082d6914d759e74f0b76da1e56b8b5
skibamichal94 <skibamichal94@gmail.com>
2018-10-26 15:25:19 +0200

    fix

--- app/src/main/java/pl/skipcode/kotlinbaseappmvvm/feature/commons/navigation/BaseRouter.kt
+++ app/src/main/java/pl/skipcode/kotlinbaseappmvvm/feature/commons/navigation/BaseRouter.kt
@@ -2,23 +2,23 @@ package pl.skipcode.kotlinbaseappmvvm.feature.commons.navigation
 
 import android.content.Intent
 import pl.skipcode.kotlinbaseappmvvm.feature.commons.BaseContract
+import pl.skipcode.kotlinbaseappmvvm.feature.commons.ui.BaseActivity
 import pl.skipcode.kotlinbaseappmvvm.feature.splash.ui.SplashActivity
 
 abstract class BaseRouter(
-        private val activityView: BaseContract.ActivityView
+        private val activity: BaseActivity
 ) : BaseContract.Router{
 
     override fun finish() {
-        activityView.finish()
+        activity.finish()
     }
 
     override fun goToSplashActivity() {
-        activityView.startActivity(SplashActivity::class.java,
+        activity.startActivity(SplashActivity::class.java,
                 listOf(
                         Intent.FLAG_ACTIVITY_NEW_TASK,
                         Intent.FLAG_ACTIVITY_CLEAR_TASK
-                ),
-                null
+                )
         )
         finish()
     }
